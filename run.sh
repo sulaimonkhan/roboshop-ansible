666f6a68f12ff669a343ee42a9782c2bb43250d4
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-11-03 00:35:28 +0530

    redis

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ if [ $# -ne 2 ]; then
   echo "Usage: bash run.sh <component-name> <env>"
   exit 1
 fi
-ansible-playbook -i $1-$2.sulaimondevopsb72.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e app_name=$1 roboshop.yml
+ansible-playbook -i $1-$2.sulaimondevopsb72.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e component=$1 roboshop.yml
   
 # chrome:- make file 18:27
